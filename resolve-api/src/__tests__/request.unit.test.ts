5cd0d78a5ba843416d4db31fbdfd89cea5001a40
max <mozg.mayhem@gmail.com>
2020-02-12 18:25:51 +0300

    Add retry on errors test

--- resolve-api/src/__tests__/request.unit.test.ts
+++ resolve-api/src/__tests__/request.unit.test.ts
@@ -3,6 +3,7 @@ import { request } from '../request'
 import { Context } from '../context'
 import determineOrigin from '../determine_origin'
 import { getRootBasedUrl } from '../utils'
+import { HttpError } from '../errors'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -22,7 +23,8 @@ const mFetch = jest.fn(() => ({
   status: 200,
   headers: {
     get: (): void => undefined
-  }
+  },
+  text: (): Promise<string> => Promise.resolve('response')
 }))
 const mDetermineOrigin = mocked(determineOrigin)
 const mGetRootBasedUrl = mocked(getRootBasedUrl)
@@ -77,6 +79,24 @@ test('global fetch called', async () => {
   })
 })
 
+test('http error thrown with response text', async () => {
+  const fetchResult = mFetch()
+  mFetch.mockClear()
+
+  mFetch.mockReturnValueOnce({
+    ...fetchResult,
+    ok: false,
+    status: 500,
+    text: () => Promise.resolve('error-text')
+  })
+
+  await expect(
+    request(mockContext, '/request', {
+      param: 'param'
+    })
+  ).rejects.toEqual(new HttpError(500, 'error-text'))
+})
+
 test('jwt set to authorization header', async () => {
   const jwtProvider = {
     get: jest.fn(() => 'j-w-t'),
@@ -102,10 +122,11 @@ test('jwt updated via provider with response header', async () => {
     set: jest.fn()
   }
   const getHeader = jest.fn(() => 'response-jwt')
+  const fetchResult = mFetch()
+  mFetch.mockClear()
 
   mFetch.mockReturnValueOnce({
-    ok: true,
-    status: 200,
+    ...fetchResult,
     headers: {
       get: getHeader
     }
@@ -117,4 +138,119 @@ test('jwt updated via provider with response header', async () => {
 
   expect(jwtProvider.set).toHaveBeenCalledWith('response-jwt')
   expect(getHeader).toHaveBeenCalledWith('x-jwt')
+})
+
+test('retry on error', async () => {
+  const fetchResult = mFetch()
+  mFetch.mockClear()
+
+  mFetch.mockReturnValueOnce({
+    ...fetchResult,
+    ok: false,
+    status: 401
+  })
+
+  const response = await request(
+    mockContext,
+    '/request',
+    {
+      param: 'param'
+    },
+    {
+      retryOnError: {
+        errors: 401,
+        attempts: 1,
+        period: 1
+      },
+      debug: false
+    }
+  )
+
+  expect(mFetch).toHaveBeenCalledTimes(2)
+  expect(response).toEqual(
+    expect.objectContaining({
+      ok: true,
+      status: 200
+    })
+  )
+})
+
+test('retry on various errors', async () => {
+  const fetchResult = mFetch()
+  mFetch.mockClear()
+
+  mFetch.mockReturnValueOnce({
+    ...fetchResult,
+    ok: false,
+    status: 401
+  })
+  mFetch.mockReturnValueOnce({
+    ...fetchResult,
+    ok: false,
+    status: 500
+  })
+
+  const response = await request(
+    mockContext,
+    '/request',
+    {
+      param: 'param'
+    },
+    {
+      retryOnError: {
+        errors: [401, 500],
+        attempts: 2,
+        period: 1
+      },
+      debug: false
+    }
+  )
+
+  expect(mFetch).toHaveBeenCalledTimes(3)
+  expect(response).toEqual(
+    expect.objectContaining({
+      ok: true,
+      status: 200
+    })
+  )
+})
+
+test('fail on unexpected errors', async () => {
+  const fetchResult = mFetch()
+  mFetch.mockClear()
+
+  mFetch.mockReturnValueOnce({
+    ...fetchResult,
+    ok: false,
+    status: 500
+  })
+  mFetch.mockReturnValueOnce({
+    ...fetchResult,
+    ok: false,
+    status: 401
+  })
+
+  await expect(
+    request(
+      mockContext,
+      '/request',
+      {
+        param: 'param'
+      },
+      {
+        retryOnError: {
+          errors: 500,
+          attempts: 1,
+          period: 1
+        },
+        debug: false
+      }
+    )
+  ).rejects.toEqual(
+    expect.objectContaining({
+      code: 401
+    })
+  )
+
+  expect(mFetch).toHaveBeenCalledTimes(2)
 })
