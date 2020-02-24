f27200773d4a50dfc2de6e1dce43b53ea6f8c404
Vasily Sokolov <vasiliy.sokolov@devexpress.com>
2020-02-24 21:59:18 +0300

    test env update

--- client/App.js
+++ client/App.js
@@ -6,6 +6,42 @@ import Test from './containers/Test'
 
 import CommentTree from './comments/CommentTree'
 
+const Item = ({ aggregateId }) => {
+  const [online, setOnline] = useState(1)
+
+  const handleSetOnline = () => {
+    setOnline(true)
+  }
+  const handleSetOffline = () => {
+    setOnline(false)
+  }
+
+  return (
+    <React.Fragment>
+      <div style={{ borderTop: '1px solid gray', marginBottom: '20px'}}>
+        <button
+          onClick={() => {
+            handleSetOnline()
+          }}
+        >
+          Go online
+        </button>
+        <button
+          onClick={() => {
+            handleSetOffline()
+          }}
+        >
+          Go offline
+        </button>
+        <code>
+          <pre>{aggregateId}</pre>
+        </code>
+        {online && <Test aggregateId={aggregateId} />}
+      </div>
+    </React.Fragment>
+  )
+}
+
 const App = ({ staticPath }) => {
   const stylesheetLink = {
     rel: 'stylesheet',
@@ -23,30 +59,17 @@ const App = ({ staticPath }) => {
     content: 'width=device-width, initial-scale=1'
   }
 
-  const [online, setOnline] = useState(true)
-
   return (
     <div>
       <div>
         <Helmet title="ReSolve Hooks Example" link={links} meta={[meta]} />
       </div>
-      <button
-        onClick={() => {
-          setOnline(true)
-        }}
-      >
-        Go online
-      </button>
-
-      <button
-        onClick={() => {
-          setOnline(false)
-        }}
-      >
-        Go offline
-      </button>
-
-      {online && <Test />}
+      <Item aggregateId="e526cfa9-6ab2-4ec5-bce6-4a0bd4bb6e4d" />
+      <Item aggregateId="6a29bfb6-495e-4d5e-9162-392025b7c887" />
+      {/* <Item aggregateId="1012029a-94f3-41ca-9536-94e18d91a388" />
+      <Item aggregateId="95887203-5173-4151-aeda-d85654b42910" />
+      <Item aggregateId="95887203-5173-4151-aeda-d85654b42910" />
+      <Item aggregateId="*" /> */}
     </div>
   )
 }
--- client/containers/Test.js
+++ client/containers/Test.js
@@ -3,9 +3,8 @@ import React, { useState } from 'react'
 import { useSubscription } from 'resolve-react-hooks'
 
 const viewModelName = 'myViewModel'
-const aggregateId = 'e526cfa9-6ab2-4ec5-bce6-4a0bd4bb6e4d' //'63adaa7d-bcf0-4d0f-bb22-3dce823c6019'
 
-function Test() {
+function Test({ aggregateId }) {
   const [eventsCount, setEventsCount] = useState(0)
   const [subscribed, setSubscribed] = useState(false)
 
@@ -19,7 +18,7 @@ function Test() {
     setSubscribed(true)
   }
 
-  useSubscription(viewModelName, [aggregateId], handleEvent, handleSubscribe)
+  useSubscription(viewModelName, aggregateId === '*' ? '*' : [aggregateId], handleEvent, handleSubscribe)
 
   return (
     <React.Fragment>
